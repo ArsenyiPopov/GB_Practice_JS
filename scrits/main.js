afcc3fe417f7427f35bd838f3d410efb439d039f
Arsnyi <arsenyi.popov@gmail.com>
2023-07-18 00:44:30 +0300

    GB_DZ_2_JS

--- scrits/main.js
+++ scrits/main.js
@@ -1,6 +1,6 @@
-//if (confirm("вы уверены?")) {
-  //  alert ("Hello workd");
-//}
+// if (confirm("вы уверены?")) {
+//     alert ("Hello workd");
+// }
 
 //else {
   //  alert("Всем пока!");
@@ -16,15 +16,34 @@ let element = document.getElementById("el");
 // выводим консоль
 console.log(element);
 //проверка на наличие класса
-if (element.classList.contains("gold"))
-    // Удаляем класс
-    element.classList.remove("gold");
-else   
+// if (element.classList.contains("gold"))
+//     // Удаляем класс
+//     element.classList.remove("gold");
+// else   
 
-    element.classList.add("gold");
+//     element.classList.add("gold");
+//проверка на наличие класса (как условие выше)
+element.classList.toggle("gold");
+element.textContent = element.textContent == "Just Do It" ? "Think Different" : "Just Do It";
 }
 
 
 let button = document.getElementById("main_btn");
 console.log(button);
 button.addEventListener('click', changedColor)
+
+let name = prompt("Как вас зовут?",);
+
+alert(`Добро пожаловать, ${name}`); 
+
+
+let age = prompt("Сколько вам лет?");
+switch(true){
+  case 0 < age && age < 18 : 
+      alert("Ты еще ребенок!");
+  break;
+  case 18 < age && age < 30 : 
+      alert("Добро пожаловать!");
+  break;
+}
+
